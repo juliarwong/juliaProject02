0ba531e05f19a59a3b9405b29ee1e754ab1c0c2f
juliarwong <wong.julia.r@gmail.com>
2023-04-04 08:44:58 -0400

    added event listener for large brewery button and added a list all button

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const myApp = {
   // create a function that will fetch the micro breweries data and display it on the page
   microBtn() {
     const apiUrl =
-      "https://api.openbrewerydb.org/v1/breweries?by_type=micro&per_page=3";
+      "https://api.openbrewerydb.org/v1/breweries?by_type=micro&per_page=12";
 
     fetch(apiUrl)
       .then((response) => response.json())
@@ -39,7 +39,42 @@ const myApp = {
 
   brewpubBtn() {
     const apiUrl =
-      "https://api.openbrewerydb.org/v1/breweries?by_type=brewpub&per_page=3";
+      "https://api.openbrewerydb.org/v1/breweries?by_type=brewpub&per_page=12";
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const dataContainer = document.getElementById("dataContainer");
+        dataContainer.innerHTML = ""; // Clear the container
+        data.forEach((item) => {
+          const itemElement = document.createElement("div");
+          itemElement.innerHTML = `<h2>${item.name}</h2><p>${item.street}, ${item.city}, ${item.state}</p>`;
+          dataContainer.appendChild(itemElement);
+        });
+      })
+      .catch((error) => console.error(error));
+  },
+
+  largeBreweryBtn() {
+    const apiUrl =
+      "https://api.openbrewerydb.org/v1/breweries?by_type=large&per_page=12";
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const dataContainer = document.getElementById("dataContainer");
+        dataContainer.innerHTML = ""; // Clear the container
+        data.forEach((item) => {
+          const itemElement = document.createElement("div");
+          itemElement.innerHTML = `<h2>${item.name}</h2><p>${item.street}, ${item.city}, ${item.state}</p>`;
+          dataContainer.appendChild(itemElement);
+        });
+      })
+      .catch((error) => console.error(error));
+  },
+
+  allBtn() {
+    const apiUrl = "https://api.openbrewerydb.org/v1/breweries";
 
     fetch(apiUrl)
       .then((response) => response.json())
@@ -66,3 +101,11 @@ microButton.addEventListener("click", myApp.microBtn);
 // Add event listener to the brewpub button
 const brewpubButton = document.getElementById("brewpub");
 brewpubButton.addEventListener("click", myApp.brewpubBtn);
+
+// Add event listener to the large brewery button
+const largeBreweryButton = document.getElementById("large");
+largeBreweryButton.addEventListener("click", myApp.largeBreweryBtn);
+
+// Add event listener to the all brewery button
+const allButton = document.getElementById("all");
+allButton.addEventListener("click", myApp.allBtn);
